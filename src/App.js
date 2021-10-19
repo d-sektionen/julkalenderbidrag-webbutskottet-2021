b227e3942b9d7df1495ea9498226b09d40a8e32c
Michelle Krejci <mickr592@student.liu.se>
2021-10-19 18:39:40 +0200

    Fixed timer, made images non-draggable and fixed some styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react"
-import { MemoryRouter, Redirect, Route, Switch } from "react-router"
-import "./App.css"
-import "./components/Card"
-import "./components/Header"
+import { useEffect, useState } from "react";
+import { MemoryRouter, Redirect, Route, Switch } from "react-router";
+import "./App.css";
+import "./components/Card";
+import "./components/Header";
 
-import Card from "./components/Card"
-import Header from "./components/Header"
-import Victory from "./pages/Victory"
-import Menu from "./pages/Menu"
-import Scoreboard from "./pages/Scoreboard"
-import Snowflakes from "./components/Snowflakes"
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Victory from "./pages/Victory";
+import Menu from "./pages/Menu";
+import Scoreboard from "./pages/Scoreboard";
+import Snowflakes from "./components/Snowflakes";
 
 const cards = [
   {
     name: "Isak Horvath (Webmaster)",
     img: "images/isse.png",
-    description: "Programmeringsintreserad person som pluggar sitt tredje år på U-programmet!",
+    description:
+      "Programmeringsintreserad person som pluggar sitt tredje år på U-programmet!",
   },
   {
     name: "Viktor Holta",
@@ -25,7 +26,8 @@ const cards = [
   {
     name: "Martin Kuiper",
     img: "images/martin.jpeg",
-    description: "Martin gillar att bygga coola saker och sjunga karaoke. Han pluggar IP2 och har en dödsfejd med css.",
+    description:
+      "Martin gillar att bygga coola saker och sjunga karaoke. Han pluggar IP2 och har en dödsfejd med css.",
   },
   {
     name: "Erik Ekelöf",
@@ -45,70 +47,70 @@ const cards = [
   {
     name: "Michelle Krejci",
     img: "images/mich.png",
-    description: "här kan man lägga till beskrivning",
+    description: "Programmering och pengar är fett kul.",
   },
   {
     name: "Felix Lindgren",
     img: "https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1632&q=80",
     description: "här kan man lägga till beskrivning",
   },
-]
+];
 
 const shuffle = (array) => {
-  let currentIndex = array.length
-  let randomIndex
+  let currentIndex = array.length;
+  let randomIndex;
 
   while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex--
-    ;[array[currentIndex], array[randomIndex]] = [
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
-    ]
+    ];
   }
-  return array
-}
+  return array;
+};
 
 function App() {
-  const [memoryDeck, setMemoryDeck] = useState([])
-  const [openCards, setOpenCards] = useState([])
-  const [finishedCards, setFinishedCards] = useState([])
-  const [moves, setMoves] = useState(0)
+  const [memoryDeck, setMemoryDeck] = useState([]);
+  const [openCards, setOpenCards] = useState([]);
+  const [finishedCards, setFinishedCards] = useState([]);
+  const [moves, setMoves] = useState(0);
 
-  const [startTime, setStartTime] = useState(new Date())
-  const [currentTime, setCurrentTime] = useState(new Date())
-  const [timerActive, setTimerActive] = useState(false)
-  const [liuid, setLiuid] = useState("1")
-  const [name, setName] = useState("2")
+  const [startTime, setStartTime] = useState(new Date());
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [timerActive, setTimerActive] = useState(false);
+  const [liuid, setLiuid] = useState("1");
+  const [name, setName] = useState("2");
 
   const resetCards = () => {
-    setFinishedCards([])
-    setOpenCards([])
-    setMoves(0)
-    setMemoryDeck(shuffle([...cards, ...cards]))
-  }
+    setFinishedCards([]);
+    setOpenCards([]);
+    setMoves(0);
+    setMemoryDeck(shuffle([...cards, ...cards]));
+  };
 
   const resetTimer = () => {
-    setStartTime(new Date())
+    setStartTime(new Date());
     setInterval(() => {
       if (timerActive) {
-        setCurrentTime(new Date())
+        setCurrentTime(new Date());
       }
-    }, 1000)
-    setTimerActive(true)
-  }
+    }, 1000);
+    setTimerActive(true);
+  };
 
   // const cards = Array(6*3).fill('')
 
   const handleClick = (data) => {
     if (openCards.length === 1 && openCards[0] !== data) {
-      setOpenCards((prev) => [...prev, data])
-      setMoves((moves) => moves + 1)
+      setOpenCards((prev) => [...prev, data]);
+      setMoves((moves) => moves + 1);
     } else {
       // clear timer?
-      setOpenCards([data])
+      setOpenCards([data]);
     }
-  }
+  };
 
   // Check if cards flipped are the same
   useEffect(() => {
@@ -116,112 +118,104 @@ function App() {
       openCards.length === 2 &&
       memoryDeck[openCards[0]].name === memoryDeck[openCards[1]].name
     ) {
-      setFinishedCards((finishedCards) => [...finishedCards, ...openCards])
+      setFinishedCards((finishedCards) => [...finishedCards, ...openCards]);
     }
-  }, [openCards, memoryDeck])
+  }, [openCards, memoryDeck]);
 
-  // Varje par behöver ha, två bilder, två beskrivningar
-  const generateCardPair = (cards) => {
-    let card1 = {}
-    let card2 = {}
-    card1.description = cards.description1
-    card1.img = cards.img1
-    card1.turned = false
-    card1.completed = false
-    cards.push(card1)
+  useEffect(() => {
+    setTimerActive(true);
+  }, []);
 
-    card2.description = cards.description2
-    card2.img = cards.img2
-    card2.turned = false
-    card2.completed = false
-    cards.push(card2)
-    // lägger till två kort med, {id, bild, vänd: bool} i cards
-  }
-
-  const cardSets = cards.length
-  const cardSetsLeft = cardSets - finishedCards.length / 2
-  
   const FetchLiuId = async () => {
-    let headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Origin': '',authorization:"Bearer " + window.localStorage.getItem("token")}
-    let options = {method:"get", headers: new Headers(headers)}
-    await fetch("https://backend.d-sektionen.se/account/me", options).
-      then(res => res.json()).
-      then((data) => {
-        setLiuid(data.username)
-        setName(data.pretty_name)
-      })
-  }
+    let headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Origin: "",
+      authorization: "Bearer " + window.localStorage.getItem("token"),
+    };
+    let options = { method: "get", headers: new Headers(headers) };
+    await fetch("https://backend.d-sektionen.se/account/me", options)
+      .then((res) => res.json())
+      .then((data) => {
+        setLiuid(data.username);
+        setName(data.pretty_name);
+      });
+  };
 
   const sendScore = async () => {
     /* let headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Origin': ''}
     let options = {method:"post", headers: new Headers(headers), mode: 'cors'} */
     let data = JSON.stringify({
-    user: liuid,
-    time_taken: (currentTime-startTime),
-    total_tries: moves
-    })
-    let url_local = "http://localhost:8000"
-    let url_heroku = "https://webbu-julkalender-21.herokuapp.com"
+      user: liuid,
+      time_taken: currentTime - startTime,
+      total_tries: moves,
+    });
+    let url_local = "http://localhost:8000";
+    let url_heroku = "https://webbu-julkalender-21.herokuapp.com";
     await fetch(url_heroku + "/scoreboard/create/", {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
       headers: {
-        'Content-Type': 'application/json',
-        'authorization' :"Bearer " + window.localStorage.getItem("token")
+        "Content-Type": "application/json",
+        authorization: "Bearer " + window.localStorage.getItem("token"),
       },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    }).then(res => res.json()).
-    then((data) => {
-      console.log(data)
-    }) 
-        
-  }
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
   const urlParams = new URLSearchParams(window.location.search);
-  const access = urlParams.get("access")
-  if(access != null){
-    window.localStorage.setItem("token", access)
-    window.history.replaceState({}, document.title, "/")
-    FetchLiuId()
-  } else if(window.localStorage.getItem("token") != "null" || window.localStorage.getItem("token") != null){
-    FetchLiuId()
+  const access = urlParams.get("access");
+  if (access != null) {
+    window.localStorage.setItem("token", access);
+    window.history.replaceState({}, document.title, "/");
+    FetchLiuId();
+  } else if (
+    window.localStorage.getItem("token") != "null" ||
+    window.localStorage.getItem("token") != null
+  ) {
+    FetchLiuId();
   }
 
-  
+  const cardSets = cards.length;
+  const cardSetsLeft = cardSets - finishedCards.length / 2;
+  const timeValue = currentTime - startTime;
 
   return (
-    <div className='App'>
+    <div className="App">
       <Snowflakes />
       <MemoryRouter>
         <Switch>
-          <Route exact path='/'>
-            <Menu 
+          <Route exact path="/">
+            <Menu
               onStart={() => {
-                resetTimer()
-                resetCards()
+                resetTimer();
+                resetCards();
               }}
             />
           </Route>
-          <Route path='/scoreboard'>
+          <Route path="/scoreboard">
             <Scoreboard />
           </Route>
-          <Route path='/game'>
-            
+          <Route path="/game">
             <Header
               name={name}
               liuid={liuid}
-              time={currentTime - startTime}
+              time={timeValue < 0 ? 0 : timeValue}
               correct={finishedCards.length / 2}
               left={cardSetsLeft}
               guesses={moves}
             />
             <button onClick={sendScore}>Send score</button>
-            <div className='container'>
-              <div className='card-area'>
+            <div className="container">
+              <div className="card-area">
                 {cardSetsLeft > 0 ? (
                   memoryDeck.map(({ img, name, description }, i) => (
                     <Card
@@ -230,25 +224,25 @@ function App() {
                       name={name}
                       profileDescription={description}
                       img={img}
-                      description='Back'
+                      description="Back"
                       isFlipped={() => openCards.includes(i)}
                       isMatched={() => finishedCards.includes(i)}
                       handleCardClick={() => handleClick(i)}
                     />
                   ))
                 ) : (
-                  <Redirect to='/victory' />
+                  <Redirect to="/victory" />
                 )}
               </div>
             </div>
           </Route>
-          <Route path='/victory'>
+          <Route path="/victory">
             <Victory
               time={currentTime - startTime}
               guesses={moves}
               onRestart={() => {
-                resetTimer()
-                resetCards()
+                resetTimer();
+                resetCards();
               }}
             />
           </Route>
@@ -257,7 +251,7 @@ function App() {
 
       <div></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,34 @@
-import React from "react"
+import React from "react";
 
-import "./Card.css"
+import "./Card.css";
 
 class Card extends React.Component {
   render() {
-    const { isFlipped, handleCardClick, isMatched, name, profileDescription, img } = this.props;
+    const {
+      isFlipped,
+      handleCardClick,
+      isMatched,
+      name,
+      profileDescription,
+      img,
+    } = this.props;
 
-    return (  
+    return (
       <div
-        className={`card ${isFlipped() ? "card-show" : ""} ${isMatched() ? "card-show card-matched" : ""} `}
+        className={`card ${isFlipped() ? "card-show" : ""} ${
+          isMatched() ? "card-show card-matched" : ""
+        } `}
         onClick={() => handleCardClick()}
+        draggable="false"
       >
-        <div className='card-content'>
+        <div className="card-content">
           <h3>{name}</h3>
           <p>{profileDescription}</p>
-          <img src={img} alt={name} className="card-img" />
+          <img src={img} alt={name} className="card-img" draggable="false" />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Card
+export default Card;
